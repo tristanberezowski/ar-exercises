5676548c0711737403c53b8df691cce28496e866
Tristan <tristanberezowski@gmail.com>
2019-03-19 21:28:37 +0000

    2 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,7 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.where(id: 1).first
+@store2 = Store.where(id: 2).first
+@store1.name = "Deep Cove"
+@store1.save
