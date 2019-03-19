04de4aad6d528616a06a4d0ebd704cdac978e6cd
Tristan <tristanberezowski@gmail.com>
2019-03-19 21:31:10 +0000

    3 complete

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,8 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.where(id: 3).first
+@store3.destroy
+
+storeCount = Store.count
+puts "There are #{storeCount} stores in the database"
