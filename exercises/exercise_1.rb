746a1f4fbc3eac3beb82ed66888abf60211b7716
Tristan <tristanberezowski@gmail.com>
2019-03-19 21:20:31 +0000

    first exercise finished

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,16 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# Task: use create class method to make 3 stores
+
+# class Store
+#   attr_accessor :annual_revenue
+
+
+# end
+
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+storeCount = Store.count
+puts "There are #{storeCount} stores in the database"
