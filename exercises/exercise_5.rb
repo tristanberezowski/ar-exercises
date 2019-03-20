25ceb753a15cc53a110d6e9b5b8c61eed7bc32b7
Tristan <tristanberezowski@gmail.com>
2019-03-20 01:01:11 +0000

    5 done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,15 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total = Store.sum("annual_revenue")
+rich = Store.where("annual_revenue > ?", 1000000).count
+storeNum = Store.count
+
+print "Total Annual Revenue: $"
+puts total
+
+print "Average Annual Revenue: "
+puts total / storeNum
+
+print "Stores earning more than $1M: "
+puts rich
